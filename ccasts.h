262005222e65afa9f677cbd9c7c3312756c3478a
Michael Builov <mbuilov@gmail.com>
2017-04-11 14:28:40 +0300

    remove RAW_CONTAINER_OF

--- ccasts.h
+++ ccasts.h
@@ -28,11 +28,6 @@
    NULL     -> NULL
    M*       -> C*
    const M* -> const C*
-
-  RAW_CONTAINER_OF:
-   NULL     -> invalid
-   M*       -> C*
-   const M* -> C*
 */
 
 #include <stddef.h>
@@ -89,9 +84,9 @@ static inline void *_const_cast_void__(const void *p)
 */
 #define CONST_CAST(type, ptr) ((type*)_const_cast_void__((ptr) + 0*sizeof(((const type*)(const void*)(ptr) - (ptr)))))
 
-A_Nonnull_all_args A_Const_function A_Check_return A_Ret_never_null A_Ret_valid A_Ret_range(==,(char*)p - offset)
+A_Nonnull_all_args A_Const_function A_Check_return A_Ret_never_null A_Ret_range(==,(char*)p - offset)
 static inline void *_container_of__(
-	A_Notnull A_At((char*)p - offset, A_Readable_bytes(offset)) const void *p/*!=NULL*/,
+	A_Notnull A_At((char*)p - offset, A_Writable_bytes(offset)) const void *p/*!=NULL*/,
 	size_t offset)
 {
 #if defined __GNUC__ && __GNUC__ >= 6
@@ -105,9 +100,9 @@ static inline void *_container_of__(
 	return (char*)_const_cast_void__(p) - offset;
 }
 
-A_Const_function A_Check_return A_Ret_opt_valid A_When(p, A_Ret_range(==,(char*)p - offset))
+A_Const_function A_Check_return A_When(!p, A_Ret_null) A_When(p, A_Ret_notnull A_Ret_range(==,(char*)p - offset))
 static inline void *_opt_container_of__(
-	A_When(p, A_At((char*)p - offset, A_Readable_bytes(offset))) const void *p/*NULL?*/,
+	A_When(p, A_At((char*)p - offset, A_Writable_bytes(offset))) const void *p/*NULL?*/,
 	size_t offset)
 {
 	return p ? (char*)_const_cast_void__(p) - offset : NULL;
@@ -120,25 +115,19 @@ static inline void *_opt_container_of__(
 #define _check_constness_(ptr, member, x) (x)
 #endif
 
-/* ptr must be != NULL and must point to member of initialized object */
+/* ptr must be != NULL and must point to member of allocated object */
 /* NOTE: must specify const type for const ptr */
 #define CONTAINER_OF(ptr/*!=NULL*/, type, member) \
 	_check_constness_(ptr, member, \
 		((type*)_container_of__(ptr, offsetof(type, member) + \
 			0*sizeof((&((type*)_const_cast_void__(ptr))->member - (ptr))))))
 
-/* ptr may be NULL, else must point to member of initialized object; returns NULL if ptr is NULL */
+/* ptr may be NULL, else must point to member of allocated object; returns NULL if ptr is NULL */
 /* NOTE: must specify const type for const ptr */
 #define OPT_CONTAINER_OF(ptr/*NULL?*/, type, member) \
 	_check_constness_(ptr, member, \
 		((type*)_opt_container_of__(ptr, offsetof(type, member) + \
 			0*sizeof((&((type*)_const_cast_void__(ptr))->member - (ptr))))))
-
-/* ptr may be NULL and may point to uninitialized object */
-/* NOTE: also does const-cast - may get non-const pointer from a const one */
-#define RAW_CONTAINER_OF(ptr/*NULL?*/, type, member) \
-	((type*)_const_cast_void__((const char*)(ptr) - offsetof(type, member) + \
-		0*sizeof((&((type*)_const_cast_void__(ptr))->member - (ptr)))))
 
 #ifdef __cplusplus
 }
