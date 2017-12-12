ba9651d4cc5776fd31a3f7774c174e4ab8c9a87b
Michael Builov <mbuilov@gmail.com>
2017-12-12 13:39:56 +0300

    make test more complex

--- tests/get_opt_test.mk
+++ tests/get_opt_test.mk
@@ -19,8 +19,9 @@ SRC := get_opt_test.c
 
 # generate rules for testing built executable and creating output file
 # save the name of generated output file (the same as $(addsuffix .out,$(call ALL_TARGETS,EXE)))
-OUT := $(call DO_TEST_EXE_RET,1 -gaf --help 3 --file 2 -b3 --level=4 -l 5 -d6 --debug 7 -o 8 -vr --output=9\
-  -fg -f g --verbose 1 --verbose=4 -g 3 -g1 -9 - --trace --trace q --trace=v -t -tra -trace -h 6 -ogv- -- 4 -b --y 9)
+OUT := $(call DO_TEST_EXE_RET,$(call SHELL_ESCAPE,! $$ ^ ' " % ^ 1 2 $(comment)) -gaf\
+  --help 3 --file 2 -b3 --level=4 -l 5 -d6 --debug 7 -o 8 -vr --output=9 -fg -f g\
+  --verbose 1 --verbose=4 -g 3 -g1 -9 - --trace --trace q --trace=v -t -tra -trace -h 6 -ogv- -- 4 -b --y 9)
 
 # generate a rule for comparing $(OUT) and get_opt_test.out
 $(call DO_CMP_TEXT,$(OUT),get_opt_test.out)
