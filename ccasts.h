eb3b0e2c433ea68ac289f493f7fc29fadfee916e
Michael Builov <mbuilov@gmail.com>
2019-01-09 17:41:54 +0300

    fix macros, add CAST_PPC, CAST_PP

--- ccasts.h
+++ ccasts.h
@@ -3,7 +3,7 @@
 
 /**********************************************************************************
 * C-casts
-* Copyright (C) 2012-2018 Michael M. Builov, https://github.com/mbuilov/cmn_headers
+* Copyright (C) 2012-2019 Michael M. Builov, https://github.com/mbuilov/cmn_headers
 * Licensed under Apache License v2.0, see LICENSE.TXT
 **********************************************************************************/
 
@@ -13,9 +13,11 @@
 
   "pointer->pointer casts"
 
-  CAST          | A*       -> B*
-  CAST_CONSTANT | const A* -> const B*
-  CONST_CAST    | const A* -> A*
+  CAST          | A*        -> B*
+  CAST_CONSTANT | const A*  -> const B*
+  CONST_CAST    | const A*  -> A*
+  CAST_PPC      | const A** -> const B**
+  CAST_PP       | const A** -> B*, const A*** -> B*, const A*const ** -> B*, ...
 
   "member->container casts"
 
@@ -37,60 +39,92 @@
 extern "C" {
 #endif
 
-/* for casting pointer to one type to pointer to another type */
+/* used in place of void* */
+struct c_casts_void_;
+
+/* for casting pointer to one type -> pointer to another type */
 A_Const_function A_Check_return A_Ret_range(==,p)
-static inline void *c_cast_void_(void *p)
+static inline struct c_casts_void_ *c_cast_void_(void *p)
 {
-	return p;
+	return (struct c_casts_void_*)p;
 }
 
-/* for casting const pointer to one type to const pointer to another type */
+/* for casting pointer to one const type -> pointer to another const type */
 A_Const_function A_Check_return A_Ret_range(==,p)
-static inline const void *c_cast_constant_void_(const void *p)
+static inline const struct c_casts_void_ *c_cast_constant_void_(const void *p)
 {
-	return p;
+	return (const struct c_casts_void_*)p;
 }
 
-/* for casting const pointer to non-const one to the same type */
+/* for removing 'constness' of the pointed type */
 A_Const_function A_Check_return A_Ret_range(==,p)
-static inline void *c_const_cast_void_(const void *p)
+static inline struct c_casts_void_ *c_const_cast_void_(const void *p)
 {
 #if defined(__GNUC__) && (__GNUC__ > 4 || (4 == __GNUC__ && __GNUC_MINOR__ >= 2))
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wcast-qual" /* warning: cast from type 'const void*' to type 'void*' casts away qualifiers */
 #endif
-	return (void*)p;
+	return (struct c_casts_void_*)p;
 #if defined(__GNUC__) && (__GNUC__ > 4 || (4 == __GNUC__ && __GNUC_MINOR__ >= 2))
 #pragma GCC diagnostic pop
 #endif
 }
 
-/* cast non-const pointer to one type to const/non-const pointer to another type:
+/* cast pointer to one non-const type -> pointer to another const/non-const type:
 
   struct my_type1 *p;
   struct my_type2 *q = CAST(struct my_type2, p);
 */
-/* compiler will generate a warning if 'ptr' is const */
-#define CAST(type, ptr) ((type*)c_cast_void_(ptr))
+/* compiler will generate a warning if 'ptr' points to const type */
+#define CAST(type_, ptr_) ((type_*)c_cast_void_(ptr_))
 
-/* cast const/non-const pointer to one type to const pointer to another type:
+/* cast pointer to one const/non-const type -> pointer to another const type:
 
   const struct my_type1 *p;
   const struct my_type2 *q = CAST_CONSTANT(const struct my_type2, p);
 */
 /* compiler will generate a warning if 'type' is non-const */
-#define CAST_CONSTANT(type, ptr) ((type*)c_cast_constant_void_(ptr))
+#define CAST_CONSTANT(type_, ptr_) ((type_*)c_cast_constant_void_(ptr_))
 
-/* cast const pointer to non-const one for the same type:
+/* remove 'constness' of the pointed type:
 
   const struct my_type *p;
   struct my_type *q = CONST_CAST(struct my_type, p);
 */
 /* compiler will generate a warning if type of 'ptr' is not 'type' */
-#define CONST_CAST(type, ptr) ((type*)c_const_cast_void_((ptr) + 0*sizeof(((const type*)(const void*)(ptr) - (ptr)))))
+#define CONST_CAST(type_, ptr_) \
+  ((void)sizeof(((const type_*)(const void*)(ptr_) - (ptr_))), (type_*)c_const_cast_void_(ptr_))
+
+/* cast pointer to non-const pointer to one const/non-const type -> pointer to non-const pointer to another const type:
+
+  const struct my_type1 **pp;
+  const struct my_type2 **qq = CAST_PPC(const struct my_type2, pp);
+*/
+/* compiler will generate an error if 'pp' is not a pointer (to pointer) */
+#ifndef _MSC_VER
+/* compiler will generate a warning if 'type' is non-const */
+/* compiler will generate a warning if 'pp' points to const pointer */
+#define CAST_PPC(type_, pp_) ((void)sizeof((type_*)c_cast_constant_void_(*(pp_))), (type_**)c_cast_void_(pp_))
+#else
+/* C++ compiler will generate an error if 'type' is non-const */
+#define CAST_PPC(type_, pp_) ((void)sizeof(c_cast_constant_void_(*(pp_))), (type_**)c_const_cast_void_(pp_))
+#endif
+
+/* cast pointer to non-const pointer to some type -> pointer to given non-const type:
+
+  const struct my_type1 **pp;
+  struct my_type2 *qq = CAST_PP(struct my_type2, pp);
+*/
+/* compiler will generate an error if 'pp' is not a pointer (to pointer) */
+#ifndef _MSC_VER
+/* compiler will generate a warning if 'pp' points to const pointer */
+#define CAST_PP(type_, pp_) ((void)sizeof(c_cast_constant_void_(*(pp_))), CAST(type_, pp_))
+#else
+#define CAST_PP(type_, pp_) ((void)sizeof(c_cast_constant_void_(*(pp_))), (type_*)c_const_cast_void_(pp_))
+#endif
 
 A_Nonnull_all_args A_Const_function A_Check_return A_Ret_never_null A_Ret_range(==,(char*)p - offset)
-static inline void *c_container_of_(
+static inline struct c_casts_void_ *c_container_of_(
 	A_Notnull A_At((char*)p - offset, A_Writable_bytes(offset)) const void *p/*!=NULL*/,
 	size_t offset)
 {
@@ -102,15 +136,15 @@ static inline void *c_container_of_(
 #if defined __GNUC__ && __GNUC__ >= 6
 #pragma GCC diagnostic pop
 #endif
-	return (char*)c_const_cast_void_(p) - offset;
+	return (struct c_casts_void_*)((char*)c_const_cast_void_(p) - offset);
 }
 
 A_Const_function A_Check_return A_When(!p, A_Ret_null) A_When(p, A_Ret_notnull A_Ret_range(==,(char*)p - offset))
-static inline void *c_opt_container_of_(
+static inline struct c_casts_void_ *c_opt_container_of_(
 	A_When(p, A_At((char*)p - offset, A_Writable_bytes(offset))) const void *p/*NULL?*/,
 	size_t offset)
 {
-	return p ? (char*)c_const_cast_void_(p) - offset : NULL;
+	return (struct c_casts_void_*)(p ? (char*)c_const_cast_void_(p) - offset : NULL);
 }
 
 #ifdef __cplusplus
@@ -119,33 +153,33 @@ static inline void *c_opt_container_of_(
 
 /* check that CONTAINER_OF() doesn't discard const qualifier */
 #if defined __cplusplus && __cplusplus >= 201103L
-#define c_check_constness_(ptr, member, x) ((x) + 0*sizeof(*(decltype(&(x)->member)*)0 = (ptr)))
+#define c_check_constness_(ptr_, member_, x_) ((void)sizeof(*(decltype(&(x_)->member_)*)0 = (ptr_)), (x_))
 #elif defined __GNUC__
-#define c_check_constness_(ptr, member, x) ((x) + 0*sizeof(*(__typeof__(&(x)->member)*)0 = (ptr)))
+#define c_check_constness_(ptr_, member_, x_) ((void)sizeof(*(__typeof__(&(x_)->member_)*)0 = (ptr_)), (x_))
 #else
-#define c_check_constness_(ptr, member, x) (x)
+#define c_check_constness_(ptr_, member_, x_) (x_)
 #endif
 
 #if defined _MSC_VER && defined __cplusplus
 /* if type is constant, msvc2017 C++ compiler crashes while evaluating standard offsetof() from <stddef.h> */
 template <class T> struct c_casts_offsetof_1 {typedef T XT;};
-#define c_casts_offsetof_(type, member) offsetof(c_casts_offsetof_1<type>::XT, member)
+#define c_casts_offsetof_(type_, member_) offsetof(c_casts_offsetof_1<type_>::XT, member_)
 #else
-#define c_casts_offsetof_(type, member) offsetof(type, member)
+#define c_casts_offsetof_(type_, member_) offsetof(type_, member_)
 #endif
 
 /* ptr must be != NULL and must point to member of allocated object */
 /* NOTE: must specify const type for const ptr */
-#define CONTAINER_OF(ptr/*!=NULL*/, type, member) \
-	c_check_constness_(ptr, member, \
-		((type*)c_container_of_(ptr, c_casts_offsetof_(type, member) + \
-			0*sizeof((&((type*)c_const_cast_void_(ptr))->member - (ptr))))))
+#define CONTAINER_OF(ptr_/*!=NULL*/, type_, member_) \
+	c_check_constness_(ptr_, member_, \
+		((type_*)c_container_of_(ptr_, c_casts_offsetof_(type_, member_) + \
+			0*sizeof((&((type_*)c_const_cast_void_(ptr_))->member_ - (ptr_))))))
 
 /* ptr may be NULL, else must point to member of allocated object; returns NULL if ptr is NULL */
 /* NOTE: must specify const type for const ptr */
-#define OPT_CONTAINER_OF(ptr/*NULL?*/, type, member) \
-	c_check_constness_(ptr, member, \
-		((type*)c_opt_container_of_(ptr, c_casts_offsetof_(type, member) + \
-			0*sizeof((&((type*)c_const_cast_void_(ptr))->member - (ptr))))))
+#define OPT_CONTAINER_OF(ptr_/*NULL?*/, type_, member_) \
+	c_check_constness_(ptr_, member_, \
+		((type_*)c_opt_container_of_(ptr_, c_casts_offsetof_(type_, member_) + \
+			0*sizeof((&((type_*)c_const_cast_void_(ptr_))->member_ - (ptr_))))))
 
 #endif /* CCASTS_H_INCLUDED */
